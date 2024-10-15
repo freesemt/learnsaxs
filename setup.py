9609799aa40e6b4e7ff54c6c723816567fc615e8
freesemt <freesemt@gmail.com>
2024-10-15 17:04:45 +0900

    Update setup.py
    updated the version to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='learnsaxs',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     author="Masatsuyo Takahashi",
     author_email='freesemt@gmail.com',
